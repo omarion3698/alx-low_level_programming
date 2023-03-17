ccde0743b7b55ca479960d5bc446189daa7d66e6
omarion3698 <omaribinbakarivic@gmail.com>
2023-03-17 22:26:00 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all possible combinations of single-digit numbers
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 10)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
