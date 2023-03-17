7f2decf034d1888d73cf30ed283bb492339cb3c6
omarion3698 <omaribinbakarivic@gmail.com>
2023-03-17 21:38:05 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Prints single digit numbers of base 10 starting from 0 on newline
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
