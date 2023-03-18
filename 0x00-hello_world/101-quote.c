7ad3d239784e998f46341d0575d0deb0f1435b2c
omarion3698 <omaribinbakarivic@gmail.com>
2023-03-18 12:46:42 +0000

    fixing betty style of code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - prints exactly the same text given in the task
+  * main - Prints exactly the same text
   *
   * Return: Always 1
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return 1;
+	fputs("and that piece of art is useful\" -
+			Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
