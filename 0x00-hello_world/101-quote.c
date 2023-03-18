92b8865f3466a731e0731c8555f2321bc7a1c20b
omarion3698 <omaribinbakarivic@gmail.com>
2023-03-18 12:40:11 +0000

    fixing fputs function's errors

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - prints exactly the same text given in the task
+  *
+  * Return: Always 1
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return 1;
+}
