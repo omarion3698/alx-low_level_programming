06706040e422878aa4e64d85972632410d595bda
omarion3698 <omaribinbakarivic@gmail.com>
2023-03-24 22:34:04 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-				if (j >= 10)
+			if (j >= 10)
+			{
 				_putchar('1');
 				_putchar(j % 10 + '0');
-		}
-		`		_putchar('\n');
+			}
+			_putchar('\n');
 	}
 }
