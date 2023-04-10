d4e0191035d1e71b9c7a80ba6b4db420b6134c09
omarion3698 <omaribinbakarivic@gmail.com>
2023-04-10 10:43:02 +0000

    prints the number of arguments passed into the program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - checks the code correctness I guess
+  * @argv: one of the parameters used for printing
+  * @argc: the other parameter used for printing
+  * Return: Always 0.
+  */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
