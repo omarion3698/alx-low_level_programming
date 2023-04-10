39e51b7d29f73aae54c6dbccfd288361ab0e6f16
omarion3698 <omaribinbakarivic@gmail.com>
2023-04-10 10:38:23 +0000

    prints the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main -  writes the name
+  * @argc: counts the arguments
+  * @argv: points to array of strings
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
